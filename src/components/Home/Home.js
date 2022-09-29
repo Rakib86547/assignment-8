94be1a0d32d191e2e963a36d8f37aa625d6a2e74
Rakib86547 <contact.antor.bd@gmail.com>
2022-09-29 07:25:31 +0600

    activities added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
+import logo from '../../image/logo.png';
 import './Home.css';
 const Home = () => {
+    const [activities, setActivities] = useState([])
+    useEffect(() => {
+        fetch('data.json')
+        .then(rest => rest.json())
+        .then(data => setActivities(data))
+    }, [])
     return (
         <div className='parent-home-conainer'>
             <div className="activity-container">
-                <h1>this is activity container</h1>
+                <img src={logo} alt="" />
+                <h1>Antor Feetnes Club</h1>
+                {
+                    activities.map(activity => console.log(activity))
+                }
             </div>
 
             <div className="details-container"></div>

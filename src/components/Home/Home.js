bf90014cae656ef178f4eaac0a9ba8e8d1f45f1e
Rakib86547 <contact.antor.bd@gmail.com>
2022-09-29 07:03:23 +0600

    home added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        <div className='parent-home-conainer'>
+            <div className="activity-container">
+                <h1>this is activity container</h1>
+            </div>
+
+            <div className="details-container"></div>
+        </div>
+    );
+};
+
+export default Home;

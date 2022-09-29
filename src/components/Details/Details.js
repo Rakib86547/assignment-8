b66b439474b59af16ccfed8c54552949586ac86f
Rakib86547 <contact.antor.bd@gmail.com>
2022-09-29 10:21:25 +0600

    details section addedd

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -13,9 +13,26 @@ const Details = () => {
             </div>
 
             <div className="body-detai">
-                
+                <h3>55kg <br /><small>Weight</small></h3>
+                <h3>6.7 <br /><small>Height</small></h3>
+                <h3>22yrs <br /><small>age</small></h3>
             </div>
 
+            <div className="break-section">
+                <h3>Add a Break</h3>
+                <div className="break-btn">
+                    <button>10s</button>
+                    <button>20s</button>
+                    <button>30s</button>
+                    <button>40s</button>
+                </div>
+            </div>
+
+            <div className="exercise-details">
+                <h5>Exercise Time: </h5>
+                <h5>Break Time: </h5>
+                <button className='activity-btn'>Activity Complete</button>
+            </div>
         </div>
     );
 };
